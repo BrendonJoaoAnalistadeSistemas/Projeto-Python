bf7d488d46cd859cfb456096a545f14072628e0f
BrendonJoaoAnalistadeSistema <brendonanalistadesistemas@gmail.com>
2024-06-19 21:38:33 -0300

    #002 - Estrutura do menu - início

---
+++ sistema/menu/2_menu.py
@@ -0,0 +1,26 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Autor: Brendon João Campos Neves.
+# Data início: 19/06/2024.
+# Data final: 
+# Objetivo: Menu do sistema com a junção dos arquivos 3, 4, 5, 6, e 7.
+
+# Bibliotecas:
+import os
+
+# Limpando o terminal:
+os.system('cls')
+
+# Exibindo título:
+print('+'*79)
+print('='*79)
+print('^'*79)
+print('~'*79)
+print('°'*79)
+print('-'*79)
+print('_'*79)
+# Exbindo comandos:
+
+# Escolha, validação e verificação da opção que o usuário escolheu:
+
+# Saindo do sistema:
