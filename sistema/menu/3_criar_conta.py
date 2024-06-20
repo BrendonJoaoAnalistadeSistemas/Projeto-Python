37c8da4e6602ce95af9e24ded22e0c8c8cbf6b24
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-06-19 21:38:32 -0300

    #003 - estrutura do sistema de criaacao de conta - início

---
+++ sistema/menu/3_criar_conta.py
@@ -0,0 +1,14 @@
+# Curso: desenvolvivento de sistemas
+# Turma: 0152
+# Autor : Beatriz victoria
+# Data do inicio : 19\06
+# Data final:
+# Objetivo: criação da conta do sistema
+
+# biblioteca do sistema
+import os
+
+# limpando
+os.system('cls')
+
+# entrada dos dados
