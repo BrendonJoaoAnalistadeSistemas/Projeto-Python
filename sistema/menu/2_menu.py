0ae1861d37dfbf8c400481b8c2704013a9dbdf67
BrendonJoaoAnalistadeSistema <brendonanalistadesistemas@gmail.com>
2024-06-20 21:37:21 -0300

    #004 - Estrutura do menu - continuação

--- sistema/menu/2_menu.py
+++ sistema/menu/2_menu.py
@@ -2,25 +2,69 @@
 # Turma: 0152.
 # Autor: Brendon João Campos Neves.
 # Data início: 19/06/2024.
-# Data final: 
+# Data final:
 # Objetivo: Menu do sistema com a junção dos arquivos 3, 4, 5, 6, e 7.
 
 # Bibliotecas:
 import os
+import platform
+
+# Declarações de variávies:
+comandos = ''''''
+opcao = 0
+
 
 # Limpando o terminal:
-os.system('cls')
+if platform.system() == 'Windows':
+    os.system('cls')
+else:
+    os.system('clear')
 
 # Exibindo título:
-print('+'*79)
-print('='*79)
-print('^'*79)
-print('~'*79)
-print('°'*79)
-print('-'*79)
-print('_'*79)
+print('''
+|---------------------------|
+|                           |
+|       Bem-vindo(a)!       |
+|       Dual B Store        |
+|                           |
+|---------------------------|''')
+
 # Exbindo comandos:
+comandos = '''
+|---------------------------|
+|         Comandos:         |
+|      1 - Criar conta      |
+|      2 - Fazer Login      |
+|      3 - Ver produtos     |
+|      4 - Ver carrinho     |
+|      5 - Pagamento        |
+|---------------------------|'''
+print(comandos)
+print()
 
-# Escolha, validação e verificação da opção que o usuário escolheu:
+# Usuário escolhendo uma opção:
+while True:
+    opcao = input('Digite o número correspondente a opção que deseja executar: ')
 
-# Saindo do sistema:
+    # Validação da opção:
+    if not(opcao.isnumeric()):
+        print('Por favor insirá apenas números!')
+        continue
+    else:
+        opcao = int(opcao)
+        break
+print()
+
+# Verificando qual opção o usuário escolheu e agindo de acordo:
+if opcao == 1:
+    pass
+elif opcao == 2:
+    pass
+elif opcao == 3:
+    pass
+elif opcao == 4:
+    pass
+elif opcao == 5:
+    pass
+else:
+    pass
