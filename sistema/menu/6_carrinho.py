9f074310a5d42285ec53f456989c5067808fb1e2
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-06-28 20:46:43 -0300

    #007 - correção sincronização

---
+++ sistema/menu/6_carrinho.py
@@ -0,0 +1,28 @@
+import os 
+
+
+os.system('cls')
+
+
+print('-'*70)
+print('Adicionar produtos para compra no carrinho')
+print('='*70)
+
+carrinho = []
+
+while True:
+    os.system('cls')
+       
+    print('-'*70)
+    print('opções para compra')
+    print('='*70)
+    print('1. adicione produtos para compra')
+    print('2. visualizar produto')
+    print('3. Remover')
+    print()
+
+    item = print("Escolha um item para compra")
+
+    
+
+    
