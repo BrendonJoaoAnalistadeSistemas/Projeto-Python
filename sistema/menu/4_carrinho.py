3d1e818309e5e2bc065ae6c5feed0bac4e29f327
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-06-28 21:34:38 -0300

    # 008 - carrinho (inicialização)

---
+++ sistema/menu/4_carrinho.py
@@ -0,0 +1,33 @@
+import os 
+
+
+
+os.system('cls') 
+
+print('-'*70) 
+print('Adicionar produtos para compra no carrinho')
+print('='*70)
+
+carrinho = []
+
+while True:
+    os.system('cls')
+    print('-'*70)
+    print('opções para compra')
+    print('='*70)
+    print('1. Adicionado no carrinho.')
+    print('2. Removido do carrinho.')
+    print('3. visualizar produto.')
+    print('4. Não encontrado no carrinho.')
+    
+    item = print("Escolha um item para compra")
+    
+    if produto  == '1':
+       print('1. Adicione produtos para compra: ')
+       produto.append()
+    else:
+     print('adicione produtos ao carrinho: {produto} ')
+    
+    if produto == '2':
+     insert.remuve.itens()
+    print('3. Removido do carrinho')
